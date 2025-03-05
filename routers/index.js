b7e878db44a62fb78bb208c90e8bec874b3adfcc
KevinVerleyen <verleyen-kevin@hotmail.be>
2025-03-05 11:11:34 +0100

    type intervention controller router

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const clientRouter = require("./client.router");
+const typeInterventionRouter = require("./type_intervention.router");
 
 router.use("/clients", clientRouter);
+router.use("/types", typeInterventionRouter);
 
 module.exports = router;
